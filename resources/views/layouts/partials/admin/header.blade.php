2e2c78f2dfc993af5b20a2c781a3ca4382996406
feebrian <adamaditama13@gmail.com>
2024-03-06 14:56:14 +0700

    view: make admin dashboard

---
+++ resources/views/layouts/partials/admin/header.blade.php
@@ -0,0 +1,10 @@
+<header class="flex items-center w-full h-16 bg-green-300">
+    <div class="container mx-auto">
+        <label for="side-nav" class="btn btn-square btn-sm btn-ghost drawer-button lg:hidden"><svg
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+        </label>
+    </div>
+</header>
---
+++ resources/views/layouts/partials/admin/nav.blade.php
@@ -0,0 +1,42 @@
+<div class="drawer-side">
+    <label for="side-nav" aria-label="close sidebar" class="drawer-overlay"></label>
+    <ul class="w-56 min-h-full menu text-base-content bg-base-200">
+        <li class="flex items-center">
+            <img src="{{ asset('img/logo/logo.png') }}" alt="" class="w-32">
+        </li>
+        <li>
+            <details open>
+                <summary class="font-semibold"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122" />
+                    </svg>
+                    Management</summary>
+                <ul>
+                    <li>
+                        <details open>
+                            <summary class="font-semibold">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9" />
+                                </svg>
+                                Buku
+                            </summary>
+                            <ul>
+                                <li>
+                                    <a href="{{ route('admin.books.index') }}">Booklists</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('admin.books.create') }}">Create</a>
+                                </li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li><a>Submenu 2</a></li>
+                </ul>
+            </details>
+        </li>
+        <li><a>Item 3</a></li>
+    </ul>
+</div>
