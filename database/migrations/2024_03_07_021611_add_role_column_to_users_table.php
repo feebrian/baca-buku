95d1df5e51c98caddb16c5195dd498b4bf894d1b
feebrian <adamaditama13@gmail.com>
2024-03-08 07:10:25 +0700

    db: update user role migration

--- database/migrations/2024_03_07_021611_add_role_column_to_users_table.php
+++ database/migrations/2024_03_07_021611_add_role_column_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['peminjam', 'petugas', 'admin']);
+            $table->enum('role', ['peminjam', 'petugas', 'admin'])->nullable();
         });
     }
 
